2e6c1d486a0ac2de366e90768232d4a3ec8b810c
jacktooandroid <9480172+jacktooandroid@users.noreply.github.com>
2020-07-03 17:58:44 +0800

    Add files via upload

--- SmokePing/Smokeping_Bootstrap.sh
+++ SmokePing/Smokeping_Bootstrap.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo apt-get update && sudo apt-get install smokeping -y
+sudo apt-get update && sudo apt-get install smokeping miniupnpc -y
 sudo wget https://raw.githubusercontent.com/jacktooandroid/smokeping/master/SmokePing/Probes.txt -O /etc/smokeping/config.d/Probes
 sudo curl https://raw.githubusercontent.com/jacktooandroid/smokeping/master/SmokePing/Probes.txt -o /etc/smokeping/config.d/Probes
 sudo wget https://raw.githubusercontent.com/jacktooandroid/smokeping/master/SmokePing/Presentation.txt -O /etc/smokeping/config.d/Presentation
@@ -17,3 +17,12 @@ sudo service apache2 restart
 #Downloading Cloudflare DDNS Script
 sudo wget https://raw.githubusercontent.com/jacktooandroid/cloudflare/master/cloudflare_ddns_modified-smokeping.sh -O /home/cloudflare_ddns_modified-smokeping.sh
 sudo curl https://raw.githubusercontent.com/jacktooandroid/cloudflare/master/cloudflare_ddns_modified-smokeping.sh -o /home/cloudflare_ddns_modified-smokeping.sh
+
+#MiniUPNP Settings
+echo "sudo upnpc -r 80 tcp" | sudo tee -a /home/miniupnp-smokeping.sh
+echo "sudo upnpc -r 8080 tcp" | sudo tee -a /home/miniupnp-smokeping.sh
+echo "sudo upnpc -r 53 tcp" | sudo tee -a /home/miniupnp-smokeping.sh
+echo "sudo upnpc -r 53 udp" | sudo tee -a /home/miniupnp-smokeping.sh
+sudo bash /home/miniupnp-smokeping.sh
+
+exit
